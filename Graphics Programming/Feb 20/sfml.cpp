28609ed39ddfdaeb7f7d0ba7fcb7af63ce3d5bf0
Sai <drathee954@gmail.com>
2026-02-24 11:56:00 +0530

    Update sfml.cpp

--- Graphics Programming/Feb 20/sfml.cpp
+++ Graphics Programming/Feb 20/sfml.cpp
@@ -1,1 +1,18 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
 
+int main()
+{
+    VideoMode vm(1920, 1080);
+    RenderWindow window(vm, "Namaskaram");
+
+    // Game loop
+    while (window.isOpen())
+    {
+        window.clear();
+
+        // draw objects
+        window.display();
+    }
+    return 0;
+}
